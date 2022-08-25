5b11889acc56f30696f88ca3fe9008f9d3bd075d
saquib <saquibali534@gmail.com>
2022-08-26 01:53:14 +0530

    add plotly

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os
 import numpy as np
+import os
 from PIL import Image
 from flask import Flask,flash, render_template, request, redirect, session, send_from_directory
 from flask import redirect, send_file, url_for
@@ -17,8 +17,7 @@ import torch
 import torch.nn as nn
 import rasterio
 import cv2
-
-import pandas as pd
+import json
 import json
 import plotly
 import plotly.express as px
@@ -48,12 +47,17 @@ transformsmain = transforms.Compose([
 
 def out_img(img):
 	img = normalize(img)
+	print(img.shape)
 	# k = int(np.random.randint(0,2500))
 	# img = img[:,k:k+128,k:k+128]
+	
 	img  = torch.from_numpy(img)
 	img = transformsmain(img)
+	print(img.shape)
 	m = nn.UpsamplingBilinear2d(scale_factor=2)
+
 	img = img.unsqueeze(0)
+	print(img.shape)
 	img = img.float()
 	bi_out = m(img)
 	out = model(img)
@@ -75,6 +79,7 @@ def normalize(im):
 	MAX_H = im.max()
 	im = (im - MIN_H)/(MAX_H-MIN_H)
 	return im
+
 
 
 @app.route('/')
@@ -104,8 +109,7 @@ def upload_image():
 		cv2.imwrite(f'static/uploads/trial.png', img)
 		img = Image.fromarray(img,'RGB')
 		img.save("static/uploads/temp.jpeg")
-		out_plt=out_im
-		print(out_plt.shape)
+		out_plt = out_im
 		out_im = np.transpose(out_im[0], (1,2,0))
 		out_im = out_im*255
 		out_im = out_im.astype(int)
@@ -114,7 +118,10 @@ def upload_image():
 		bi_out = bi_out*255
 		bi_out= bi_out.astype(int)
 		cv2.imwrite(f'static/uploads/bi_out.png', bi_out)
+		# out_im = Image.fromarray(out_im, 'RGB')
+		# out_im.save("static/uploads/out.jpeg")
 		flash('Image successfully uploaded and displayed below')
+		
 
 		x = np.linspace(0,1,512)
 		y = np.linspace(0,1,512)
@@ -142,7 +149,7 @@ def upload_image():
 
 
 		return render_template('index.html', filename='temp.jpeg', graphJSON=graphJSON)
-		# return render_template('index.html', graphJSON=graphJSON)
+		
 	else:
 		flash('Allowed image types are -> png, jpg, jpeg, gif, tiff')
 		return redirect(request.url)
